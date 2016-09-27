7f9edad73f1567d97dd8ad8ce0b8240701e4ebc1
Kamil <kamiltor@wp.pl>
2016-09-27 15:53:45 +0200

    First Commit

---
+++ SmsListConverter/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SmsListConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_filename = new System.Windows.Forms.TextBox();
+            this.char_1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.char_2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.char_3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.debug = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(110, 119);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(112, 23);
+            this.btn_convert.TabIndex = 0;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File name";
+            // 
+            // txt_filename
+            // 
+            this.txt_filename.Location = new System.Drawing.Point(112, 13);
+            this.txt_filename.Name = "txt_filename";
+            this.txt_filename.Size = new System.Drawing.Size(110, 20);
+            this.txt_filename.TabIndex = 3;
+            // 
+            // char_1
+            // 
+            this.char_1.Location = new System.Drawing.Point(112, 39);
+            this.char_1.Name = "char_1";
+            this.char_1.Size = new System.Drawing.Size(18, 20);
+            this.char_1.TabIndex = 5;
+            this.char_1.Tag = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Char to convert";
+            // 
+            // char_2
+            // 
+            this.char_2.Location = new System.Drawing.Point(112, 65);
+            this.char_2.Name = "char_2";
+            this.char_2.Size = new System.Drawing.Size(18, 20);
+            this.char_2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Char after convert";
+            // 
+            // char_3
+            // 
+            this.char_3.Location = new System.Drawing.Point(112, 91);
+            this.char_3.Name = "char_3";
+            this.char_3.Size = new System.Drawing.Size(18, 20);
+            this.char_3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Char replaced";
+            // 
+            // debug
+            // 
+            this.debug.Location = new System.Drawing.Point(14, 119);
+            this.debug.Name = "debug";
+            this.debug.Size = new System.Drawing.Size(65, 23);
+            this.debug.TabIndex = 10;
+            this.debug.Text = "Debug";
+            this.debug.UseVisualStyleBackColor = true;
+            this.debug.Visible = false;
+            this.debug.Click += new System.EventHandler(this.debug_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 151);
+            this.Controls.Add(this.debug);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.char_3);
+            this.Controls.Add(this.char_2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.char_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_filename);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_convert);
+            this.Name = "Form1";
+            this.Tag = "Name of file ";
+            this.Text = "Sms list converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_filename;
+        private System.Windows.Forms.TextBox char_1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox char_2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox char_3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button debug;
+    }
+}
+
---
+++ SmsListConverter/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+// using System.Diagnostics;
+
+namespace SmsListConverter
+{
+    public partial class Form1 : Form
+    {
+        string filename = null;
+        string char1 = null, char2 = null, char3 = null;
+
+
+        // ****************************************************************
+        // ret: 1 - poprawny poczatek wersa, 0 - kontynuacja
+        static bool VerseTestStartLine(string verse)
+        {
+            int size;
+
+            if (verse == "Date,Time,Type,Number,Name,Message")      // linijka specialna poczatkowa
+                return true;
+
+            size = verse.Length;
+
+            if (size < 20)
+                return false;
+
+            if (verse[0] != '2')
+                return false;           // nie poczatek
+
+            if (verse[1] != '0')
+                return false;
+
+            if (verse[4] != '-')
+                return false;
+
+            if (verse[7] != '-')
+                return false;
+
+            if (verse[10] != ',')
+                return false;
+
+            if (verse[13] != ':')
+                return false;
+
+            if (verse[16] != ':')
+                return false;
+
+            if (verse[19] != ',')
+                return false;
+
+            return true;
+
+        }   // VerseTestStartLine
+
+
+        // ****************************************************************
+        // ret: 1 - special znak, 0 - nit special znak
+        static bool TestSpecial(char znak)
+        {
+            byte i;
+            char[] tab = {   (char)0xD83C, (char)55357, (char)0xDE0A, (char)0xDE09,
+                             (char)0xDE18, (char)0xDE42, (char)0xDE0D, (char)0xDE03,
+                             (char)0xDE00, (char)0xDE13, (char)0xDE12, (char)0xDCA9,
+                             (char)0xDC8F, (char)0xDC8B, (char)0xDC69, (char)0xDC36,
+                             (char)0xDC25, 
+                             (char)0 };
+
+            for (i = 0; i < 100; i++)
+            {
+                if (tab[i] == 0)
+                    break;  // return false;
+
+                if (tab[i] == znak)
+                    return true;
+            }
+
+            if (znak >= 0xDC23)
+                // if (znak >= 0xDE00)
+                return true;
+
+            return false;
+
+        }   // TestSpecial
+
+
+        // *********************************************************
+        private void MakeDefaultIniFiles()
+        {
+            TextWriter plik_wy = new StreamWriter("params.ini");
+            plik_wy.WriteLine("sms.csv");
+            plik_wy.WriteLine(",");
+            plik_wy.WriteLine(";");
+            plik_wy.WriteLine("_");
+            plik_wy.Close();
+
+        }   // MakeDefaultIniFiles
+
+
+        // *********************************************************
+        private void LoadParams()
+        {
+            TextReader plik_we = null;
+            try
+            {
+                plik_we = new StreamReader("params.ini");
+            }
+            catch
+            {
+                MakeDefaultIniFiles();
+                plik_we = new StreamReader("params.ini");
+            }
+
+            filename = plik_we.ReadLine();
+            char1 = plik_we.ReadLine();
+            char2 = plik_we.ReadLine();
+            char3 = plik_we.ReadLine();
+            plik_we.Close();
+
+            txt_filename.Text = filename;
+            char_1.Text = char1;
+            char_2.Text = char2;
+            char_3.Text = char3;
+
+        }   // LoadParams
+
+
+        // *********************************************************
+        private void SaveParams()
+        {
+            TextWriter plik_wy = new StreamWriter("params.ini");
+            plik_wy.WriteLine(txt_filename.Text);
+            plik_wy.WriteLine(char_1.Text);
+            plik_wy.WriteLine(char_2.Text);
+            plik_wy.WriteLine(char_3.Text);
+            plik_wy.Close();
+
+        }   // SaveParams
+
+
+        // *********************************************************
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadParams();
+
+        }   // Form1
+
+
+        // *********************************************************
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            string verse = null;
+            string verse_out = null;
+            short i, index = 0;
+            char znak;
+            byte comma_cnt;
+
+            // Przepisanie parametrow z okna
+            filename = txt_filename.Text;
+            char1 = char_1.Text;
+            char2 = char_2.Text;
+            char3 = char_3.Text;
+
+            SaveParams();
+
+            // otwarcie pliku do odczytu
+            StreamReader plik_we = File.OpenText(filename);
+
+            // otwarcie pliku do zapisu
+            TextWriter plik_wy = new StreamWriter("converted.txt");
+
+            verse_out = null;
+            comma_cnt = 0;
+
+            for (; ;)        // przelot po wersach
+            {
+                verse = plik_we.ReadLine();
+                if (verse == null)               // koniec pliku
+                {
+                    if (verse_out != null)
+                        plik_wy.WriteLine(verse_out);
+                    break;
+                }
+
+                index = 0;
+
+                if (VerseTestStartLine(verse))      // verse poprawnie sie zaczyna
+                {
+                    if (verse_out != null)
+                        plik_wy.WriteLine(verse_out);
+
+                    verse_out = null;
+                    comma_cnt = 0;
+                }
+                else      // vers jest kontynuacja
+                {
+                    verse_out += " ";       // dodanie spacji zeby wersy byly oddzielone
+                }
+
+                for (i = 0; i < verse.Length; i++)       // przelot po poszczegolnych znakach
+                {
+                    znak = verse[index++];
+
+                    // if (znak == ',')
+                    if (znak == char1[0])       // ','
+                    {
+                        if (comma_cnt < 5)
+                        {
+                            // verse_out += ";";
+                            verse_out += char2;
+                            ++comma_cnt;
+                        }
+                        else
+                        {
+                            verse_out += znak;
+                        }
+                    }
+                    // else if (znak == ';')       
+                    else if (znak == char2[0])   // jak w wejsciu jest srednik
+                    {
+                        // verse_out += '_';
+                        verse_out += char3;
+                    }
+                    else if (znak == '\"')
+                    {
+                        verse_out += " ";
+                    }
+                    else if (znak == '\n')
+                    {
+                        break;
+                    }
+                    else if (TestSpecial(znak))
+                        verse_out += " ";
+                    else
+                    {
+                        verse_out += znak;
+                    }
+
+                }
+
+            }
+
+            plik_we.Close();
+            plik_wy.Close();
+
+            MessageBox.Show("Conversion success", "info", MessageBoxButtons.OK);
+
+        }
+
+        private void debug_Click(object sender, EventArgs e)
+        {
+ /*
+            string test = "test";
+            char [] oko;
+            char znak;
+            string test2 = null;
+
+            // test = new string "test";
+
+            // oko = test.ToCharArray();
+            // znak = oko[0];
+
+            // znak = test.to
+            // znak = test2.to
+            znak = char_1.GetCharFromPosition(;
+            // znak = char_1[0];
+            // znak = test.ToCharArray(0, 1);
+*/ 
+        }
+    }
+}
