744b0683634f6d8d4f6328b2e41ef66a26d13b4f
Kamil <kamiltor@wp.pl>
2018-01-01 22:25:28 +0100

    Comit prostujacy

--- SmsListConverter/Form1.Designer.cs
+++ SmsListConverter/Form1.Designer.cs
@@ -42,8 +42,12 @@
 			this.label7 = new System.Windows.Forms.Label();
 			this.label8 = new System.Windows.Forms.Label();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
 			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -136,42 +140,64 @@
 			// label7
 			// 
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(4, 56);
+			this.label7.Location = new System.Drawing.Point(6, 58);
 			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(168, 13);
+			this.label7.Size = new System.Drawing.Size(60, 13);
 			this.label7.TabIndex = 15;
-			this.label7.Text = "kodowania 65001 Unicode UTF-8";
+			this.label7.Text = "File / Open";
 			// 
 			// label8
 			// 
 			this.label8.AutoSize = true;
-			this.label8.Location = new System.Drawing.Point(3, 41);
+			this.label8.Location = new System.Drawing.Point(5, 43);
 			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(174, 13);
+			this.label8.Size = new System.Drawing.Size(64, 13);
 			this.label8.TabIndex = 14;
-			this.label8.Text = "Do Excela konwertujemy używając ";
+			this.label8.Text = "OpenOffice:";
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.Controls.Add(this.label12);
+			this.groupBox1.Controls.Add(this.label9);
+			this.groupBox1.Controls.Add(this.label10);
+			this.groupBox1.Controls.Add(this.label11);
 			this.groupBox1.Controls.Add(this.label6);
 			this.groupBox1.Controls.Add(this.label5);
 			this.groupBox1.Controls.Add(this.label8);
 			this.groupBox1.Controls.Add(this.label7);
-			this.groupBox1.Location = new System.Drawing.Point(13, 163);
+			this.groupBox1.Location = new System.Drawing.Point(13, 154);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(235, 99);
+			this.groupBox1.Size = new System.Drawing.Size(235, 161);
 			this.groupBox1.TabIndex = 16;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Info";
 			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(6, 103);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(99, 13);
+			this.label10.TabIndex = 20;
+			this.label10.Text = "I wybieramy średnik";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(6, 89);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(189, 13);
+			this.label11.TabIndex = 19;
+			this.label11.Text = "Kodowanie UTF8 (na samym dole listy)";
+			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(3, 72);
+			this.label6.Location = new System.Drawing.Point(4, 74);
 			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(103, 13);
+			this.label6.Size = new System.Drawing.Size(134, 13);
 			this.label6.TabIndex = 17;
-			this.label6.Text = "(na samym dole listy)";
+			this.label6.Text = "Typ pliku: Text CSV (*.csv)";
 			// 
 			// label5
 			// 
@@ -182,11 +208,29 @@
 			this.label5.TabIndex = 16;
 			this.label5.Text = "Z Androida konwertujemy: SMS to Text";
 			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(6, 138);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(168, 13);
+			this.label9.TabIndex = 17;
+			this.label9.Text = "kodowania 65001 Unicode UTF-8";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(6, 125);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(36, 13);
+			this.label12.TabIndex = 21;
+			this.label12.Text = "Excel:";
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(312, 282);
+			this.ClientSize = new System.Drawing.Size(312, 329);
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.debug);
 			this.Controls.Add(this.label4);
@@ -228,6 +272,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-    }
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label12;
+	}
 }
 
